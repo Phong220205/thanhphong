7f34e7e524b26c3c048a13b9fd27f9a20a7c0872
Phong220205 <phongtrai465@gmail.com>
2025-09-13 17:00:22 +0700

    Thêm hàm tính tuổi từ năm sinh vào demo1.js

--- demo1.js
+++ demo1.js
@@ -1,9 +1,18 @@
-// index.js
+// demo1.js
 
 // Hàm hiển thị tên và tuổi
 function hienThiTenTuoi(ten, tuoi) {
     console.log(`Họ tên: ${ten}, Tuổi: ${tuoi}`);
 }
 
+// Hàm tính tuổi từ năm sinh
+function tinhTuoi(namSinh) {
+    const namHienTai = new Date().getFullYear();
+    return namHienTai - namSinh;
+}
+
 // Gọi thử
-hienThiTenTuoi("Phong", 19);
+let ten = "Phong";
+let namSinh = 2006;
+let tuoi = tinhTuoi(namSinh);
+hienThiTenTuoi(ten, tuoi);
